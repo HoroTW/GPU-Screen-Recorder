f7f8ef174760d94088878ccf9db4fcada3c2aaf0
dec05eba <dec05eba@protonmail.com>
2020-08-29 07:30:43 +0200

    Add script for streaming while saving a local copy of the video

---
+++ twitch-stream-local-copy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Stream on twitch while also saving the video to disk locally
+
+[ "$#" -ne 5 ] && echo "usage: twitch-stream-local-copy.sh <window_id> <fps> <audio_input> <livestream_key> <local_file>" && exit 1
+./sibs-build/linux_x86_64/release/gpu-screen-recorder -w "$1" -c flv -f "$2" -a "$3" | tee -- "$5" | ffmpeg -i pipe:0 -c:v copy -f flv -- "rtmp://live.twitch.tv/app/$4"
--- twitch-stream.sh
+++ twitch-stream.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 [ "$#" -ne 4 ] && echo "usage: twitch-stream.sh <window_id> <fps> <audio_input> <livestream_key>" && exit 1
-#ismv
-sibs build --release && ./sibs-build/linux_x86_64/release/gpu-screen-recorder -w "$1" -c flv -f "$2" -a "$3" | ffmpeg -i pipe:0 -c:v copy -f flv -max_muxing_queue_size 4096 -- "rtmp://live.twitch.tv/app/$4"
+./sibs-build/linux_x86_64/release/gpu-screen-recorder -w "$1" -c flv -f "$2" -a "$3" | ffmpeg -i pipe:0 -c:v copy -f flv -- "rtmp://live.twitch.tv/app/$4"
