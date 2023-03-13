0307a054e86d4b1734557ef3a4e3a0db6b4d6585
Bernd Zeidler <zeidler.b@gmail.com>
2023-03-13 23:10:12 +0100

    feat(scripts): :sparkles: document replay save when using systemd

--- scripts/save-replay.sh
+++ scripts/save-replay.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -e
 
+## If you are not using systemd:
 killall -SIGUSR1 gpu-screen-recorder
+## Otherwise:
+#systemctl --user kill -s SIGUSR1 gpu-screen-recorder.service
+
 notify-send -t 5000 -u low -- "GPU Screen Recorder" "Replay saved"
